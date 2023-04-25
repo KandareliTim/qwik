ef187ada8cd5a21cde6b00d3d3dd2b6d26ce64be
KandareliTim <timkandareli@gmail.com>
2023-04-25 18:35:00 +0300

    base todo 5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,41 @@
 import { component$, useSignal, $, useStore } from "@builder.io/qwik";
 
 export default component$((): any => {
-  const list = useStore<{text: string}[]>([]);
-  const inputValue = useSignal('');
+  const list = useStore<{ text: string }[]>([]);
+  const inputValue = useSignal("");
 
-  const onInputChange = $((event: any) => inputValue.value = event?.target?.value)
+  // const onInputChange = $(
+  //   (event: any) => (inputValue.value = event?.target?.value)
+  // );
 
   const addTodo = $(() => {
-    list.push({text: inputValue.value})
-    inputValue.value = ''
-  })
+    list.push({ text: inputValue.value });
+    inputValue.value = "";
+  });
 
-  const renderItem = () => {
-    if (list.length) {
-      return list.map((item, index) => {
-        return <li key={index}>{item.text}</li>
-      })
-    }
-
-    return null
-  }
+  // const renderItem = () => {
+  //   if (list.length) {
+  //     return list.map((item, index) => {
+  //       return <li key={index}>{item.text}</li>;
+  //     });
+  //   }
+  //
+  //   return null;
+  // };
 
   return (
     <>
       <div>
-        <input type="text" name="text" value={inputValue.value} onInput$={onInputChange} />
+        <input
+          type="text"
+          name="text"
+          value={inputValue.value}
+          // onInput$={onInputChange}
+        />
         <button onClick$={addTodo}>Add item</button>
       </div>
 
-      <ul>
-        {renderItem()}
-      </ul>
+      {/*<ul>{renderItem()}</ul>*/}
     </>
   );
 });
